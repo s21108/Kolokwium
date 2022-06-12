313e9b190eb6fc8ce90ea0a05fa597e68ae0ac46
Patryk Zasuwa <s21108@pjwstk.edu.pl>
2022-06-12 11:28:05 +0200

    Add project files.

---
+++ Kolokwium/Models/Musician.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium.Models
+{
+    public class Musician
+    {
+        public int IdMusician { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Nickname { get; set; }
+    }
+}
